00013a90cb955b9802df52aeb98c263f8c0ce9a4
Rob Spick <robert-spick@live.co.uk>
2015-07-11 21:38:57 +0100

    Event Unit Tests

---
+++ test/EventTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendTest\EventManager;
+
+use Zend\EventManager\Event;
+
+/**
+ * @group      Zend_Stdlib
+ */
+class EventTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstructorWithArguments()
+    {
+        $name = 'foo';
+        $target = 'bar';
+        $params = ['test','param'];
+
+        $event = new Event($name, $target, $params);
+
+        $this->assertEquals($name, $event->getName());
+        $this->assertEquals($target, $event->getTarget());
+        $this->assertEquals($params, $event->getParams());
+    }
+
+    public function testSetParamsWithInvalidParameter()
+    {
+        $event = new Event('foo');
+        $this->setExpectedException('Zend\EventManager\Exception\InvalidArgumentException');
+		$event->setParams('test');
+    }
+
+    public function testGetParamReturnsDefault()
+    {
+        $event = new Event('foo', 'bar', []);
+        $default = 1;
+
+        $this->assertEquals($default, $event->getParam('foo', $default));
+    }
+
+    public function testGetParamReturnsDefaultForObject()
+    {
+        $params = new \stdClass();
+        $event = new Event('foo', 'bar', $params);
+        $default = 1;
+
+        $this->assertEquals($default, $event->getParam('foo', $default));
+    }
+
+    public function testGetParamReturnsForObject()
+    {
+        $key = 'test';
+        $value = 'value';
+        $params = new \stdClass();
+        $params->$key = $value;
+
+        $event = new Event('foo', 'bar', $params);
+        $default = 1;
+
+        $this->assertEquals($value, $event->getParam($key));
+    }
+}
