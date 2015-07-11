b8488db93bb515286031fc186f30ee6a614f28ed
Rob Spick <robert-spick@live.co.uk>
2015-07-11 23:38:23 +0100

    More coverage for FilterIterator

--- test/FilterIteratorTest.php
+++ test/FilterIteratorTest.php
@@ -35,4 +35,42 @@ class FilterIteratorTest extends \PHPUnit_Framework_TestCase
         $filterIterator->insert(null, 1);
         $this->assertNull($filterIterator->next([0, 1, 2]));
     }
+
+    public function testContainsReturnsFalseForInvalidElement()
+    {
+        $filterIterator = new FilterIterator();
+        $this->assertFalse($filterIterator->contains('foo'));
+    }
+
+    public function testContainsReturnsTrueForValidElement()
+    {
+        $filterIterator = new FilterIterator();
+        $filterIterator->insert('foo', 1);
+        $this->assertTrue($filterIterator->contains('foo'));
+    }
+
+    public function testRemoveFromEmptyQueueReturnsFalse()
+    {
+        $filterIterator = new FilterIterator();
+
+        $this->assertFalse($filterIterator->remove('foo'));
+    }
+
+    public function testRemoveInvalidItemFromQueueReturnsFalse()
+    {
+        $filterIterator = new FilterIterator();
+        $filterIterator->insert('foo', 1);
+        $filterIterator->insert('bar', 2);
+
+        $this->assertTrue($filterIterator->remove('foo'));
+    }
+
+    public function testNextReturnsNullWhenFilterChainIsEmpty()
+    {
+        $filterIterator = new FilterIterator();
+
+        $chain = new FilterIterator();
+
+        $this->assertNull($filterIterator->next([0, 1, 2], ['foo','bar'], $chain));
+    }
 }
