d1cc7402d64910eae3a69a9ff0f03fff45812931
Matthew Weier O'Phinney <matthew@zend.com>
2015-09-24 13:10:57 -0500

    Added benchmarks from #4
    This patch adds the benchmark scripts from #4 in order to allow us to determine
    changes in performance from v2 to the proposed v3.

---
+++ benchmarks/SingleEventSingleSharedListener.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ZendBench\EventManager;
+
+use Zend\EventManager\SharedEventManager;
+use Zend\EventManager\EventManager;
+use Athletic\AthleticEvent;
+
+class SingleEventSingleSharedListener extends AthleticEvent
+{
+    use TraitEventBench;
+
+    private $sharedEvents;
+
+    private $events;
+
+    public function setUp()
+    {
+        $identifiers = $this->getIdentifierList();
+        $this->sharedEvents = new SharedEventManager();
+        $this->sharedEvents->attach($identifiers[0], 'dispatch', $this->generateCallback());
+        $this->events = new EventManager();
+        $this->events->setSharedManager($this->sharedEvents);
+        $this->events->setIdentifiers([$identifiers[0]]);
+    }
+
+    /**
+     * Trigger the dispatch event
+     *
+     * @iterations 5000
+     */
+    public function trigger()
+    {
+        $this->events->trigger('dispatch');
+    }
+}
