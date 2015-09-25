e1214674042d0b8f61f1f8fbd5d63edaab483bca
Matthew Weier O'Phinney <matthew@zend.com>
2015-09-25 08:52:32 -0500

    Updated file-level docblocks
    - `@link` now references the component repository
    - `@license` now references the LICENSE.md in the component repository

--- src/EventInterface.php
+++ src/EventInterface.php
@@ -2,9 +2,9 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @link      http://github.com/zendframework/zend-eventmanager for the canonical source repository
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   https://github.com/zendframework/zend-eventmanager/blob/master/LICENSE.md
  */
 
 namespace Zend\EventManager;
--- test/TestAsset/StaticEventsMock.php
+++ test/TestAsset/StaticEventsMock.php
@@ -2,9 +2,9 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @link      http://github.com/zendframework/zend-eventmanager for the canonical source repository
  * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   https://github.com/zendframework/zend-eventmanager/blob/master/LICENSE.md
  */
 
 namespace ZendTest\EventManager\TestAsset;
@@ -12,9 +12,6 @@ namespace ZendTest\EventManager\TestAsset;
 use Zend\EventManager\SharedEventManagerInterface;
 use Zend\Stdlib\CallbackHandler;
 
-/**
- * @group      Zend_EventManager
- */
 class StaticEventsMock implements SharedEventManagerInterface
 {
     public function getListeners($id, $event = null)
