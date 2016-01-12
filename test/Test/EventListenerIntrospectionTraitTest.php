2d2ba38c2aec4d92776530aa91808a6cba33c608
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-12 17:11:22 -0600

    Updated copyright year

--- test/Test/EventListenerIntrospectionTraitTest.php
+++ test/Test/EventListenerIntrospectionTraitTest.php
@@ -3,7 +3,7 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zend-eventmanager for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-eventmanager/blob/master/LICENSE.md
  */
 
