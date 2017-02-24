d8d874aaea4b0cb5b6bbff1ccfa1e88cea27634a
KoKsPfLaNzE <kokspflanze@gmx.de>
2017-02-24 18:56:14 +0100

    added check for phpunit 5.7 fallback updated doc-block

--- test/_autoload.php
+++ test/_autoload.php
@@ -1,10 +1,12 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-component-installer for the canonical source repository
+ * @link      http://github.com/zendframework/zend-eventmanager for the canonical source repository
  * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-component-installer/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/zendframework/zend-eventmanager/blob/master/LICENSE.md
  */
 
 use PHPUnit\Framework\ExpectationFailedException;
 
-class_alias('PHPUnit_Framework_ExpectationFailedException', ExpectationFailedException::class);
+if (class_exists('PHPUnit_Framework_ExpectationFailedException')) {
+    class_alias('PHPUnit_Framework_ExpectationFailedException', ExpectationFailedException::class);
+}
